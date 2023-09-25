deabc820a156ba299cdc90ec96bf097caeb135ea
gaello <gaellodev@gmail.com>
2023-09-25 22:19:06 +0300

    Placing layout

--- src/pages/DiceHome/DiceHome.tsx
+++ src/pages/DiceHome/DiceHome.tsx
@@ -3,15 +3,26 @@ import DicePlatform from "../../containers/DicePlatform";
 const DiceHome = () => {
     return (
 
-        <div>
-            <h3> Player number </h3>
-        <div className="flex h-full w-full items-center justify-center">
-         <DicePlatform/>
-        </div>
+        <div className="flex flex-col items-center h-full justify-between">
+            <div className="flex flex-col items-center">
+                <div> Player number </div>
+                <div className="flex">
+                    <div>Player number input</div>
+                    <div>Button player number</div>
+                </div>
+
+            </div>
+            <div className="flex h-full w-full items-center justify-center">
+                <DicePlatform />
+            </div>
+
+            <div className="flex h-full w-full items-center justify-center">
+                Winner is Player 1
+            </div>
 
         </div>
-      
+
     );
-  };
-  
-  export default DiceHome;
+};
+
+export default DiceHome;
